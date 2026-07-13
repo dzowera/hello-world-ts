d7b4a880452fb20318e278cb5ac6126eb5e7f5a9
dzowera <idzowera@byupathway.edu>
2026-07-13 18:44:24 +0200

    added to allow API accessible to browsers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/connectdb";
 import userRoutes from "./routes/userRoutes";
@@ -12,9 +13,10 @@ const PORT: any = process.env.PORT || 3000
 // connect to the database
 connectDB();
 
-// middleware to parse incoming JSON requests and form data
+// middleware to parse incoming JSON requests and form data, and allow cross-origin requests
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 // Routes
 setupSwagger(app);
